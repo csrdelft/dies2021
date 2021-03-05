96bccd8e402d8d897d7b255054a974db265c2d3a
Mark Bekooy <mark.bekooy@gmail.com>
2021-03-05 15:16:53 +0100

    Voeg alle benodigde afbeeldingen toe voor dies 2021 pagina

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import './assets/style.scss'
 
 window.addEventListener('load', function () {
     document.getElementById("player").innerHTML =
-        `<iframe src="${process.env.VUE_APP_VIDEO}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        `<iframe src="https://www.youtube-nocookie.com/embed/JDAUpwCbUog?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 });
